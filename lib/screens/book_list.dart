a168fc66e88653650e63bc048b806ff3e12cf938
NavvAbhishek <abi2000navi@gmail.com>
2025-01-27 16:14:15 +0530

    add first book

--- lib/screens/book_list.dart
+++ lib/screens/book_list.dart
@@ -22,6 +22,60 @@ class BookList extends StatelessWidget {
               ))
         ],
       ),
+      body: ListView(
+        children: [
+          const SizedBox(height: 30),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Harry Potter",
+                style: TextStyle(
+                    color: Color(0xff023047),
+                    fontWeight: FontWeight.w700,
+                    fontSize: 24),
+              ),
+              Text(
+                "J.K. Rowling",
+                style: TextStyle(color: Color(0xffFFB703), fontSize: 16),
+              ),
+              SizedBox(height: 10),
+              Image.asset(
+                'assets/images/harry-potter.png',
+                width: 275,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    "Rs.4700",
+                    style: TextStyle(color: Color(0xff023047), fontSize: 18),
+                  ),
+                  SizedBox(
+                    width: 65,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xff023047),
+                        foregroundColor: Color(0xffFFB703),
+                        fixedSize: const Size(140, 50),
+                      ),
+                      child: Text(
+                        "Add to cart",
+                        style:
+                            TextStyle(color: Color(0xffFFB703), fontSize: 16),
+                      )) // Add some spacing between the text and button
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
