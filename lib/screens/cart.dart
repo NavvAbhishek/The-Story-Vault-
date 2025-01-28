4e2d73de901ac89e293285880589bc896a50dea2
NavvAbhishek <abi2000navi@gmail.com>
2025-01-28 13:14:40 +0530

    add top text

--- lib/screens/cart.dart
+++ lib/screens/cart.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class MyCart extends StatelessWidget {
   const MyCart({super.key});
@@ -7,7 +8,34 @@ class MyCart extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Text("Hii"),
+        child: Container(
+          margin: EdgeInsets.only(top: 20, left: 15, right: 15),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              IconButton(
+                  onPressed: () {
+                    Get.back();
+                  },
+                  icon: Icon(
+                    Icons.arrow_back_ios,
+                    color: Color(0xff023047),
+                    size: 32,
+                  )),
+              Expanded(
+                child: Text(
+                  "My Cart",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Color(0xff023047),
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              const SizedBox(width: 32),
+            ],
+          ),
+        ),
       ),
     );
   }
